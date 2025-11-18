cee382145ac11fcebf75118f9b3c4ce79c104b74
Xavier Sierra <143114381+Xa-Sierra@users.noreply.github.com>
2025-11-18 12:44:15 -0600

    Change shuffle parameter to False in test loader

--- train/clip_train.py
+++ train/clip_train.py
@@ -69,7 +69,7 @@ test_loader = torch.utils.data.DataLoader(
                                torchvision.transforms.Normalize(
                                  (0.1307,), (0.3081,))
                              ])),
-  batch_size=batch_size_test, shuffle=True)
+  batch_size=batch_size_test, shuffle=False)
 
 valid_loader=test_loader
 
